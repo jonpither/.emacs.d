6351472da834def724d15fa85c6b6f36df4c02dc
Jon Pither <jon.pither@gmail.com>
2012-11-20 20:18:53 +0000

    Markdown

--- init.el
+++ init.el
@@ -57,6 +57,11 @@
 ;; Parenthesis
 (show-paren-mode)
 
+;; Markdown mode
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
