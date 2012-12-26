fcd72f562e76b0ccaca386e183bc34e81ac18cd2
Jon Pither <jon.pither@gmail.com>
2012-12-26 21:59:19 +0000

    moved ac into being a submodule. popup for ac-nrepl broken as a result of upgrading stuff..

--- init.el
+++ init.el
@@ -114,6 +114,15 @@
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
 
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+
 ;; Parenthesis
 (show-paren-mode)
 (global-rainbow-delimiters-mode)
