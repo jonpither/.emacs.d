ec63df34aee4ba0eff05b9bf44aace1fca219321
Jon Pither <jon.pither@gmail.com>
2012-12-30 15:28:34 +0000

    web-mode hooks

--- jp-html.el
+++ jp-html.el
@@ -5,3 +5,10 @@
 ;; web-mode, see http://web-mode.org
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (local-set-key (kbd "RET") 'newline-and-indent)
+)
+(add-hook 'web-mode-hook  'web-mode-hook)
