c3d42c01dc121ac30de5cda891c23d2599430ebe
Jon Pither <jon.pither@ubs.com>
2013-02-15 15:12:34 +0000

    hacking paredit to avoid clash with nrepl over M-r

--- elpa/paredit-22/paredit.el
+++ elpa/paredit-22/paredit.el
@@ -450,7 +450,7 @@ Paredit behaves badly if parentheses are imbalanced, so exercise
                 paredit-splice-sexp-killing-forward
                 ("(a (b c| d e) f)"
                  "(a b c f)"))
-   ("M-r"       paredit-raise-sexp
+   ("M-R"       paredit-raise-sexp
                 ("(dynamic-wind in (lambda () |body) out)"
                  "(dynamic-wind in |body out)"
                  "|body"))
