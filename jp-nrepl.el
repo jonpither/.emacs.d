598d6ba7abb4c6606a8496382d2ada2665abe255
Jon Pither <jon.pither@gmail.com>
2013-08-19 08:37:52 +0100

    removing my nrepl stuff, should be in nrepl.el

--- jp-nrepl.el
+++ jp-nrepl.el
@@ -1,72 +1,3 @@
-(defun extract-project-name-from-dir (project-dir)
-  (if (string-match "/[^/]*?/$" project-dir)
-      (format "%s"
-	      (replace-regexp-in-string
-	       "/$" "" (substring project-dir (+ 1 (string-match "/[^/]*?/$" project-dir)))))
-    project-dir))
-
-(defun nrepl-connection-namespace ()
-  (with-current-buffer (get-buffer (nrepl-current-connection-buffer))
-    (format "%s:%s"
-	    (extract-project-name-from-dir nrepl-project-dir)
-	    nrepl-buffer-ns)))
-
-(defun nrepl-connection-infos (connection-buffer)
-  (with-current-buffer (get-buffer connection-buffer)
-    nrepl-endpoint))
-
-(defun nrepl-current-connection-infos ()
-  (nrepl-connection-infos (nrepl-current-connection-buffer)))
-
-(defun nrepl-rotate-connection-list (connection-list)
-  (append (rest connection-list)
-          (list (first connection-list))))
-
-(defun nrepl-connection-msg ()
-  (apply #'message
-         "Active nrepl connection: %s, %s:%s"
-	 (nrepl-connection-namespace)
-         (nrepl-current-connection-infos)))
-
-(defun nrepl-switch-to-next-connection ()
-  (interactive)
-  (setq nrepl-connection-list
-        (nrepl-rotate-connection-list nrepl-connection-list))
-  (nrepl-connection-msg))
-
-(defun nrepl-show-current-connection ()
-  (interactive)
-  (nrepl-connection-msg))
-
-(defun nrepl-create-repl-buffer (process)
-  "Create a repl buffer for PROCESS."
-  (nrepl-init-repl-buffer
-   process
-   (let ((buf (generate-new-buffer-name
-	       (format "*nrepl-%s*"
-		       (with-current-buffer
-			   (get-buffer (nrepl-current-connection-buffer))
-			 (extract-project-name-from-dir nrepl-project-dir))))))
-     (pop-to-buffer buf)
-     buf)))
-
-(defun nrepl-switch-to-relevant-buffer ()
-  "Switch to relevant nrepl buffer based on the current buffer"
-  (interactive)
-  (let ((project-directory (nrepl-project-directory-for (nrepl-current-dir))))
-    (if project-directory
-  (progn
-    (lexical-let ((buf (first (remove-if-not
-             (lambda (c)
-               (equal project-directory
-                (with-current-buffer (get-buffer c)
-            nrepl-project-dir)))
-             nrepl-connection-list))))
-      (setq nrepl-connection-list
-      (cons buf (delq buf nrepl-connection-list))))
-    (nrepl-switch-to-repl-buffer nil))
-      (message "Not in an NREPL project dir"))))
-
 (setq nrepl-history-file "~/.emacs.d/nrepl-history")
 
 (nrepl-project-directory-for (nrepl-current-dir))
@@ -81,7 +12,7 @@
 (global-set-key (kbd "C-c n j") 'nrepl-jack-in)
 (global-set-key (kbd "C-c n c") 'nrepl-close)
 (global-set-key (kbd "C-c n q") 'nrepl-quit)
-(global-set-key (kbd "C-c n d") 'nrepl-show-current-connection)
-(global-set-key (kbd "C-c n n") 'nrepl-switch-to-next-connection)
+(global-set-key (kbd "C-c n d") 'nrepl-current-connection-info)
+(global-set-key (kbd "C-c n n") 'nrepl-rotate-connection)
 (global-set-key (kbd "C-c n p") 'nrepl-toggle-pretty-printing)
-(global-set-key (kbd "C-c n s") 'nrepl-switch-to-relevant-buffer)
+(global-set-key (kbd "C-c n s") 'nrepl-switch-to-repl-buffer)
