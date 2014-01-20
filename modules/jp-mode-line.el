c7556955e25c3238a6759e23eb6bf5d6ebba8bfc
Jon Pither <jon.pither@gmail.com>
2014-01-20 15:59:35 +0000

    cleaning up the mode line

---
+++ modules/jp-mode-line.el
@@ -0,0 +1,23 @@
+(defvar mode-line-symbols
+  '((clojure-mode . "λ")
+    (emacs-lisp-mode . "E-L")
+    (paredit-mode . " (P)")
+    (undo-tree-mode . " U-T")
+    (eldoc-mode . " El-D")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-symbols
+        do (let* ((mode (car cleaner))
+                  (mode-str (cdr cleaner))
+                  (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+               (setcar old-mode-str mode-str))
+             ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+(add-hook 'projectile-mode-hook
+          (lambda () (setq projectile-mode-line-lighter " Proj")))
