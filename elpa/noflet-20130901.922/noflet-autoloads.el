7c0ad4ceff7ebe0e6efb0f489da446b78ca2d7ea
Jon Pither <jon.pither@gmail.com>
2014-01-21 10:40:06 +0000

    Updating packages

---
+++ elpa/noflet-20130901.922/noflet-autoloads.el
@@ -0,0 +1,18 @@
+;;; noflet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("noflet-pkg.el" "noflet.el") (21214 19713
+;;;;;;  429661 0))
+
+;;;***
+
+(provide 'noflet-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; noflet-autoloads.el ends here
