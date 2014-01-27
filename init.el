56e16fa9d4e755611358f769d47fa743eed03bac
Jon Pither <jon.pither@gmail.com>
2014-01-27 20:55:27 +0000

    load personal/private stuff

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
 ;; ------------------------------------------------------------
 
 (defvar modules-dir (expand-file-name  "modules" user-emacs-directory))
+(defvar personal-dir (expand-file-name  "personal" user-emacs-directory))
 
 (require 'package)
 (add-to-list 'package-archives
@@ -14,6 +15,10 @@
 
 (message "Loading modules in %s..." modules-dir)
 (mapc 'load (directory-files modules-dir 't "^[^#]*el$"))
+
+(when (file-exists-p personal-dir)
+  (message "Loading personal configuration files in %s..." personal-dir)
+  (mapc 'load (directory-files personal-dir 't "^[^#]*el$")))
 
 (require 'jp-mode)
 (jp-global-mode 1)
