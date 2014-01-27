71cef9444a7c1534e938487cbe0f1499ebbc0f5b
Jon Pither <jon.pither@gmail.com>
2014-01-27 20:28:15 +0000

    new cider fn in progress

--- modules/jp-cider.el
+++ modules/jp-cider.el
@@ -2,10 +2,21 @@
 
 (setq cider-popup-stacktraces nil)
 (setq cider-repl-popup-stacktraces t)
+(setq nrepl-host "localhost")
 
 ;; eldoc
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
-(setq cider-switch-to-repl-command 'cider-switch-to-relevant-repl-buffer)
-
 (define-key cider-repl-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+
+(setq cider-known-endpoints '(("localhost" "127.0.0.1:60611")))
+
+(defun foo ()
+  (interactive)
+  (let* ((endpoint
+          (ido-completing-read "Destination: "
+                               (mapcar (lambda (label-endpoint)
+                                         (concat (car label-endpoint) " " (cadr label-endpoint)))
+                                       cider-known-endpoints)))
+         (ip-port (split-string (cadr (split-string endpoint " ")) "\\:")))
+    (cider (car ip-port) (string-to-number (cadr ip-port)))))
