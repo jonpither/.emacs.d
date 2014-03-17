43a2cde4772d5b4c0d110b103007f3215e2789ad
Jon Pither <jon.pither@gmail.com>
2014-03-17 08:10:52 +0000

    removing custom endpoint stuff as it's now in CIDER.

--- modules/jp-cider.el
+++ modules/jp-cider.el
@@ -7,17 +7,4 @@
 ;; eldoc
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
-(define-key cider-repl-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
-
-(defcustom cider-known-endpoints nil
-  "Specify a custom endpoint."
-  :type 'alist
-  :group 'cider)
-
-(defun cider-connect-to-known-endpoint ()
-  (interactive)
-  (let* ((endpoint (split-string
-                    (ido-completing-read "Destination: " cider-known-endpoints)))
-         (ip-port (split-string (if (= (length endpoint) 1)
-                                    (car endpoint) (cadr endpoint)) "\\:")))
-    (cider (car ip-port) (string-to-number (cadr ip-port)))))
+(define-key cider-repl-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)x
