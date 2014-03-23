eb5f9f12a0bee87284240fbf6cd2d3e5e5b2011e
Jon Pither <jon.pither@gmail.com>
2014-03-23 14:18:30 +0000

    works with vendors dirs for elisp dev purposes

--- init.el
+++ init.el
@@ -6,11 +6,13 @@
 
 (defvar modules-dir (expand-file-name  "modules" user-emacs-directory))
 (defvar personal-dir (expand-file-name  "personal" user-emacs-directory))
+(defvar vendor-dir (expand-file-name "vendor" user-emacs-directory))
 
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (setq package-user-dir (concat user-emacs-directory "elpa"))
+
 (package-initialize)
 
 (message "Loading modules in %s..." modules-dir)
@@ -19,6 +21,16 @@
 (when (file-exists-p personal-dir)
   (message "Loading personal configuration files in %s..." personal-dir)
   (mapc 'load (directory-files personal-dir 't "^[^#]*el$")))
+
+;; TODO make work like prelude, my vendor stuff isn't proper yet
+;; Load vendor packages
+(dolist (f (directory-files vendor-dir))
+  (let ((name (expand-file-name f vendor-dir)))
+    (when (and (file-directory-p name)
+               (not (equal f ".."))
+               (not (equal f ".")))
+      (message "Loading vendor package %s" name)
+      (mapc 'load (directory-files name 't "^[^#]*el$")))))
 
 (require 'jp-mode)
 (jp-global-mode 1)
