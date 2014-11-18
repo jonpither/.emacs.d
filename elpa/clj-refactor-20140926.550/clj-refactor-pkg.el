f68b260cc7113ad92ce431a063dbd58e1ab8e310
Jon Pither <jon.pither@gmail.com>
2014-11-18 10:36:30 +0000

    Elpas for CIDER

---
+++ elpa/clj-refactor-20140926.550/clj-refactor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clj-refactor" "20140926.550" "A collection of clojure refactoring functions" '((s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "22") (multiple-cursors "1.2.2") (cider "0.6.0")) :keywords '("convenience"))
---
+++ elpa/company-20140930.712/company-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company" "20140930.712" "Modular text completion framework" '((emacs "24.1") (cl-lib "0.5")) :url "http://company-mode.github.io/" :keywords '("abbrev" "convenience" "matching"))
