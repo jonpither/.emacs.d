cdbadf88195ff98a85ffa022d541eb7fc51b9153
Jon Pither <jon.pither@gmail.com>
2015-08-21 10:03:01 +0100

    Helpful packages

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (setq package-user-dir (concat user-emacs-directory "elpa"))
 
 (package-initialize)
@@ -37,3 +37,23 @@
 (jp-global-mode 1)
 
 (add-hook 'after-init-hook 'global-company-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cider-prompt-for-symbol nil)
+ '(clojure-defun-indents (quote (do-at testp)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(org-agenda-files
+   (quote
+    ("~/dev/rideon/platform/doc/status.org" "~/dev/juxt/contracts/clients/bettercities/frontiercities.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
