c753d33bc10b8253eb281ca5ed38aefcac84037f
Jon Pither <jon.pither@gmail.com>
2015-10-05 09:21:05 +0100

    Removing modes

--- elpa/haskell-mode-20140120.554/haskell-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "haskell-mode" "20140120.554" "A Haskell editing mode" 'nil)
--- elpa/scala-mode-2.11.2/scala-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "scala-mode" "2.11.2" "Major mode for editing Scala code." 'nil :keywords '("scala" "languages" "oop"))
