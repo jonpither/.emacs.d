e9624215dfa9484b00c7e7744885bb9e58ace3f2
Jon Pither <jon.pither@gmail.com>
2015-11-04 20:32:18 +0100

    Updates

--- init.el
+++ init.el
@@ -36,24 +36,7 @@
 (require 'jp-mode)
 (jp-global-mode 1)
 
+(require 'org)
+(require 'ox-reveal)
+
 (add-hook 'after-init-hook 'global-company-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cider-prompt-for-symbol nil)
- '(clojure-defun-indents (quote (do-at testp)))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(org-agenda-files
-   (quote
-    ("~/dev/rideon/platform/doc/status.org" "~/dev/juxt/contracts/clients/bettercities/frontiercities.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
