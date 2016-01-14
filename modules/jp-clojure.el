86fc4b27987f1092e58a424b79b45718b3204b45
Jon Pither <jon.pither@gmail.com>
2016-01-14 14:51:19 +0000

    activate clj-refactor

--- modules/jp-clojure.el
+++ modules/jp-clojure.el
@@ -24,6 +24,13 @@
 ;;                                                (match-end 1) "λ")
 ;;                                nil))))))
 
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-o")
+                               ;; insert keybinding setup here
+                               ))
+
 (require 'clojure-mode)
 
 (put-clojure-indent 'render 1)
