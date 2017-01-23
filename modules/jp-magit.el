c3aa1e320b9ca66ca614066eb022fd9c594d95a4
Jon Pither <jon.pither@gmail.com>
2017-01-23 14:33:22 +0000

    Magit has changed so much

--- modules/jp-magit.el
+++ modules/jp-magit.el
@@ -4,33 +4,30 @@
 (use-package magit
   :bind (("C-x g" . magit-status)))
 
-;; Fullscreen magit status
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
+;; ;; Fullscreen magit status
+;; (defadvice magit-status (around magit-fullscreen activate)
+;;   (window-configuration-to-register :magit-fullscreen)
+;;   ad-do-it
+;;   (delete-other-windows))
 
-;; Exit fullscreen magit status
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
+;; ;; Exit fullscreen magit status
+;; (defun magit-quit-session ()
+;;   "Restores the previous window configuration and kills the magit buffer"
+;;   (interactive)
+;;   (kill-buffer)
+;;   (jump-to-register :magit-fullscreen))
 
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+;; (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
-;; Add an extra newline to separate commit message from git commentary
+;; ;; Add an extra newline to separate commit message from git commentary
 
-(defun magit-commit-mode-init ()
-  (when (looking-at "\n")
-    (open-line 1)))
+;; (defun magit-commit-mode-init ()
+;;   (when (looking-at "\n")
+;;     (open-line 1)))
 
-(add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
+;; (add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
 
-;; close popup when commiting
+;; ;; close popup when commiting
 
-(defadvice git-commit-commit (after delete-window activate)
-  (delete-window))
-
-;; Git messenger
-(use-package git-messenger)
+;; (defadvice git-commit-commit (after delete-window activate)
+;;   (delete-window))
