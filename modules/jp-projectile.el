b3b28414a2dd4da7939496dd864ca4340386f5c0
Jon Pither <jon.pither@gmail.com>
2019-01-16 08:53:06 +0000

    Add AG. Tweaked projectile for recent setup

--- modules/jp-projectile.el
+++ modules/jp-projectile.el
@@ -2,4 +2,7 @@
   :config
   (progn)
   (projectile-global-mode)
-  (setq projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name)))))
+  (setq projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name))))
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+(use-package ag)
