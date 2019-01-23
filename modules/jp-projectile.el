b4a63ab1113a27e3657655b4694241d767b93107
Jon Pither <jon.pither@gmail.com>
2019-01-23 09:10:29 +0000

    Projectile fixes

--- modules/jp-projectile.el
+++ modules/jp-projectile.el
@@ -5,4 +5,7 @@
   (setq projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name))))
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
+(setq projectile-enable-caching t)
+(setq projectile-indexing-method 'native)
+
 (use-package ag)
