e676708c57e7eeded9a4ef7d22ee124309ce0831
Jon Pither <jon@juxt.pro>
2021-01-12 17:11:10 +0000

    Going to take a break from projectile

--- modules/jp-projectile.el
+++ modules/jp-projectile.el
@@ -1,11 +1,11 @@
-(use-package projectile
-  :config
-  (progn)
-  (projectile-global-mode)
-  (setq projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name))))
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+;; (use-package projectile
+;;   :config
+;;   (progn)
+;; ;  (projectile-global-mode)
+;;   (setq projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name))))
+;;   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
-(setq projectile-enable-caching t)
-(setq projectile-indexing-method 'native)
+;; (setq projectile-enable-caching t)
+;; (setq projectile-indexing-method 'native)
 
 (use-package ag)
